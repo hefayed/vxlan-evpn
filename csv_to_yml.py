2ef25fedb9b95e78d8b538c35dc224c7c97ea91a
hefayed <hefayed@gmail.com>
2019-07-25 02:00:17 -0700

    initial commit

--- csv_to_yml.py
+++ csv_to_yml.py
@@ -3,7 +3,7 @@ import csv
 import yaml
 
 l2vni_data = []
-with open('data.csv', mode='r') as csvfile:
+with open('l2vni_vars.csv', mode='r') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         l2vni_data.append(row['vlan_id'])
@@ -11,6 +11,6 @@ with open('data.csv', mode='r') as csvfile:
 
 l2vni_vars = list(map(int, l2vni_data))
 
-with open('l2vni_vars.yml', 'w+') as outfile:
+with open('./roles/l2vni_overlay/vars/main.yml', 'w+') as outfile:
     outfile.write('---\n')
     outfile.write(yaml.dump({'L2VNI': l2vni_vars}, default_flow_style=False))
