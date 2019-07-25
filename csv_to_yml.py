260adfc5e3ffea15128aca1caf2893ecfaaf1767
hefayed <hefayed@gmail.com>
2019-07-25 02:48:48 -0700

    initial commit

--- csv_to_yml.py
+++ csv_to_yml.py
@@ -12,5 +12,5 @@ with open('/home/contiv/vxlan-evpn/l2vni_vars.csv', mode='r') as csvfile:
 l2vni_vars = list(map(int, l2vni_data))
 
 with open('/home/contiv/vxlan-evpn/roles/l2vni_overlay/vars/main.yml', 'a') as outfile:
-    # outfile.write('---\n')
+    
     outfile.write(yaml.dump({'vlan_id': l2vni_vars}, default_flow_style=False))
